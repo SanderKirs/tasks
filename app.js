b2bf6042247f7d18972e5ff310e15b00a3ad18bd
SanderKirs <sander.kirs@khk.ee>
2022-03-02 20:10:52 +0200

    Added delete task value from local storage

--- app.js
+++ app.js
@@ -17,9 +17,26 @@ function deleteTask(e){
     if (e.target.textContent == 'X'){
         if (confirm("Do you want to delete task?")){
             e.target.parentElement.remove();
+            task = e.target.parentElement.firstChild.textContent;
+            deleteTaskFromLocalStorage(task);
         }
     }
 
+}
+
+function deleteTaskFromLocalStorage(task){
+    let tasks;
+    if(localStorage.getItem('tasks') === null){
+        tasks = [];
+    } else {
+        tasks = JSON.parse(localStorage.getItem('tasks'));
+    }
+    tasks.forEach(function (tasksElement, index){
+        if(tasksElement === task) {
+            tasks.splice(index, 1);
+        }
+    });
+    localStorage.setItem('tasks', JSON.stringify(tasks));
 }
 
 function addTask(e){
@@ -54,6 +71,5 @@ function addTaskToLocalStorage(task){
     }
     tasks.push(task);
     localStorage.setItem('tasks', JSON.stringify(tasks));
-    console.log(tasks);
 
 }
