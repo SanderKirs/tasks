666bc20609a554953452b79f6f6b45557f8bc70a
SanderKirs <sander.kirs@khk.ee>
2022-03-02 18:19:58 +0200

    Added 'delete single task' function to tasks list

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
 const form = document.querySelector('form');
 const taskInput = document.querySelector('#task');
+const tasksList = document.querySelector('.collection');
 
 form.addEventListener('submit', addTask);
-console.log(form);
+tasksList.addEventListener('click', deleteTask);
+
+function deleteTask(e){
+    if (e.target.textContent == 'X'){
+        if (confirm("Do you want to delete task?")){
+            e.target.parentElement.remove();
+        };
+    };
+
+};
+
 
 function addTask(e){
     const task = taskInput.value;
@@ -11,6 +22,13 @@ function addTask(e){
     li.className = 'collection-item';
     const text = document.createTextNode(task);
     li.appendChild(text);
+    const link = document.createElement('a');
+    link.setAttribute('href', '#');
+    console.log(link);
+    link.appendChild(document.createTextNode('X'));
+    li.appendChild(link);
+    link.className = 'secondary-content';
+
     const ul = document.querySelector('.collection');
     ul.appendChild(li);
 
